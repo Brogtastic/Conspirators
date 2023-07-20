103d81e6b3e92e483b649bd11d5f491e1077b311
Brogtastic <mbrogdon@student.fitchburgstate.edu>
2023-07-20 01:50:37 -0400

    Room code generation and scalable window with conforming elements

--- Conspirators/Conspirators.cpp
+++ Conspirators/Conspirators.cpp
@@ -10,11 +10,11 @@ using namespace std;
 // Program main entry point
 //------------------------------------------------------------------------------------
 
-string generateRandomString() {
-	const string allowedCharacters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+string GenerateRandomString() {
+	const string allowedCharacters = "123456789ABCDEFGHIJKLMNPQRSTUVWXYZ";
 	string randomString;
-	for (int i = 0; i < 4; ++i) {
-		int random = rand() % 37;
+	for (int i = 0; i < 4; i++) {
+		int random = rand() % 32;
 		randomString += allowedCharacters[random];
 	}
 	print("\n\n\n" + randomString + "\n\n\n");
@@ -37,8 +37,8 @@ int main(void)
 	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
 	InitWindow(initialScreenWidth, initialScreenHeight, "Conspirators");
 
-	string roomCode1 = generateRandomString();
-	const char* roomCode = roomCode1.c_str();
+	string generateCode = GenerateRandomString();
+	const char* roomCode = generateCode.c_str();
 
 	
 	SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
@@ -95,7 +95,9 @@ int main(void)
 		DrawRectangleRounded(testRec, 0.2f, 2, RAYWHITE);
 		DrawRectangleRounded(testRec2, 0.2f, 2, RED);
 
-		DrawText(roomCode, 400, 400, 50, WHITE);
+		//DrawText(roomCode, 400, 400, 50, WHITE);
+		DrawText(roomCode, screenWidth / 3.200000 + xScreenMargin, screenHeight / 1.800000 + yScreenMargin, screenWidth / 25.600000, WHITE);
+
 
 		if (IsKeyPressed(KEY_R)) {
 			float myPosX = screenWidth / testRec2.x;
@@ -104,6 +106,19 @@ int main(void)
 			float mySizeY = screenHeight / testRec2.height;
 
 			print("Rectangle testRec = { screenWidth / " + to_string(myPosX) + "f + xScreenMargin, screenHeight / " + to_string(myPosY) + "f + yScreenMargin, screenWidth / " + to_string(mySizeX) + ", screenHeight / " + to_string(mySizeY) + "};");
+		}
+
+		if (IsKeyPressed(KEY_T)) {
+			float myTextPosX = screenWidth / 400.0f;
+			float myTextPosY = screenHeight / 400.0f;
+			float myFontSize = screenWidth / 50.0f;
+
+			print("DrawText(string, screenWidth / " + to_string(myTextPosX) + "f + xScreenMargin, screenHeight / " + to_string(myTextPosY) + "f + yScreenMargin, screenWidth / " + to_string(myFontSize) + "f, WHITE);");
+		}
+
+		if (IsKeyPressed(KEY_N)) {
+			generateCode = GenerateRandomString();
+			roomCode = generateCode.c_str();
 		}
 		
 		EndDrawing();
