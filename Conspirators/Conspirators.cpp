2e31296a4be6c6ead5e4ce4ac495725b3ba6987b
Brogtastic <mbrogdon@student.fitchburgstate.edu>
2023-07-19 22:06:10 -0400

    intial commit

--- Conspirators/Conspirators.cpp
+++ Conspirators/Conspirators.cpp
@@ -1,20 +1,119 @@
-// Conspirators.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include "raylib.h"
 #include <iostream>
+#include <string>
+#include <list>
+#define GetIntArrayLength(x) sizeof(x) / sizeof(int)
+using namespace std;
+#define print(x) cout << x
 
-int main()
-{
-    std::cout << "Hello World!\n";
+//------------------------------------------------------------------------------------
+// Program main entry point
+//------------------------------------------------------------------------------------
+
+string generateRandomString() {
+	const string allowedCharacters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	string randomString;
+	for (int i = 0; i < 4; ++i) {
+		int random = rand() % 37;
+		randomString += allowedCharacters[random];
+	}
+	print("\n\n\n" + randomString + "\n\n\n");
+	return randomString;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+int main(void)
+{
+	// Initialization
+	//--------------------------------------------------------------------------------------
+	const int initialScreenWidth = 1280;
+	const int initialScreenHeight = 720;
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+	int screenWidth = 1280;
+	int screenHeight = 720;
+	int realScreenWidth = 1280;
+	int realScreenHeight = 720;
+	int xScreenMargin, yScreenMargin = 0;
+
+	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+	InitWindow(initialScreenWidth, initialScreenHeight, "Conspirators");
+
+	string roomCode1 = generateRandomString();
+	const char* roomCode = roomCode1.c_str();
+
+	
+	SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+	//--------------------------------------------------------------------------------------
+
+	// Main game loop
+	while (!WindowShouldClose())    // Detect window close button or ESC key
+	{
+		// Update
+		//----------------------------------------------------------------------------------
+
+		screenHeight = GetScreenHeight();
+		screenWidth = GetScreenWidth();
+		realScreenHeight = GetScreenHeight();
+		realScreenWidth = GetScreenWidth();
+
+		
+		if (screenWidth > (screenHeight * 16) / 9) {
+			screenWidth = (screenHeight * 16) / 9;
+		}
+		else {
+			screenHeight = (screenWidth * 9) / 16;
+		}
+
+		
+		if (realScreenHeight > screenHeight) {
+			yScreenMargin = (realScreenHeight - screenHeight) / 2;
+		}
+		else {
+			yScreenMargin = 0;
+		}
+		if (realScreenWidth > screenWidth) {
+			xScreenMargin = (realScreenWidth - screenWidth) / 2;
+		}
+		else {
+			xScreenMargin = 0;
+		}
+
+		//----------------------------------------------------------------------------------
+
+		// Draw
+		//----------------------------------------------------------------------------------
+		BeginDrawing();
+
+		ClearBackground(BLACK);
+
+		//posx, posy, sizex, sizey
+		Rectangle testBG = { xScreenMargin, yScreenMargin, screenWidth, screenHeight};
+		DrawRectangleRec(testBG, BLUE);
+
+		//posx, posy, sizex, sizey
+		Rectangle testRec = { screenWidth / 11.636364f + xScreenMargin, screenHeight / 1.8f + yScreenMargin, screenWidth / 25.6f, screenHeight / 14.4f };
+		Rectangle testRec2 = { screenWidth / 7.111111f + xScreenMargin, screenHeight / 1.800000f + yScreenMargin, screenWidth / 25.600000, screenHeight / 14.400000 };
+		DrawRectangleRounded(testRec, 0.2f, 2, RAYWHITE);
+		DrawRectangleRounded(testRec2, 0.2f, 2, RED);
+
+		DrawText(roomCode, 400, 400, 50, WHITE);
+
+		if (IsKeyPressed(KEY_R)) {
+			float myPosX = screenWidth / testRec2.x;
+			float myPosY = screenHeight / testRec2.y;
+			float mySizeX = screenWidth / testRec2.width;
+			float mySizeY = screenHeight / testRec2.height;
+
+			print("Rectangle testRec = { screenWidth / " + to_string(myPosX) + "f + xScreenMargin, screenHeight / " + to_string(myPosY) + "f + yScreenMargin, screenWidth / " + to_string(mySizeX) + ", screenHeight / " + to_string(mySizeY) + "};");
+		}
+		
+		EndDrawing();
+		//----------------------------------------------------------------------------------
+	}
+
+	// De-Initialization
+	//--------------------------------------------------------------------------------------
+	CloseWindow();        // Close window and OpenGL context
+	//--------------------------------------------------------------------------------------
+
+	return 0;
+}
